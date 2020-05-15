618de69c188c469e94c071b2d7c880b43e4e4820
yuuta66jp <yuuta66jp@gmail.com>
2020-05-15 09:53:04 +0000

    [finish]モデルの検証

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,13 @@
 class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  # 検証用メソッドの作成
+  validate :validate_name_not_including_comma
+
+  private
+
+  def validate_name_not_including_comma
+    # 後置ifで検証(ぼっち演算子でnilを許可)
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
