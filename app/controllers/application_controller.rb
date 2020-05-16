92e37317cab0258b78a1ba9870f1531a3b947274
yuuta66jp <yuuta66jp@gmail.com>
2020-05-16 14:05:56 +0000

    [finish]ログイン情報取得メソッド

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # helper_methodで全てのビューからも使用できるようにする
+  helper_method :current_user
+
+  private
+
+  def current_user
+    # 自己代入演算子で代入
+    # findメソッドの場合nilを渡した際エラーが発生するためfind_byを使用(nilが返ってくる)
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
