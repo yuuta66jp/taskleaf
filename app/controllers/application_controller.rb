b30b74250791a8ec1e4f2e8a37549829cf7bce29
yuuta66jp <yuuta66jp@gmail.com>
2020-05-17 04:30:02 +0000

    [finish]ログインフィルタ設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # helper_methodで全てのビューからも使用できるようにする
   helper_method :current_user
+  before_action :login_required
 
   private
 
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
     # 自己代入演算子で代入
     # findメソッドの場合nilを渡した際エラーが発生するためfind_byを使用(nilが返ってくる)
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def login_required
+    redirect_to login_url unless current_user
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  # skip_before_actionでフィルタのスキップ
+  skip_before_action :login_required
+
   def new
   end
 
