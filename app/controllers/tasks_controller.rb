3e606a8f6776ef0c15a0887e1134b361615b9432
yuuta66jp <yuuta66jp@gmail.com>
2020-05-19 06:13:16 +0000

    [finish]ソート機能の実装(Ransack)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
 
   def index
     @q = current_user.tasks.ransack(params[:q])
-    @tasks = @q.result(distinct: true).recent
+    @tasks = @q.result(distinct: true)
   end
 
   def show
